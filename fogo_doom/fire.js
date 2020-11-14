be173825644276cd7beedba2cd9ccb8f770d8138
Thiago T. Futenma <tfutenma@hotmail.com>
2020-11-13 22:23:39 -0300

    Estrutura Fogo Doom

---
+++ fogo_doom/fire.js
@@ -0,0 +1,23 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
+
+function start() {
+
+}
+
+function createFireStructure() {
+
+}
+
+function calculateFirePropagation() {
+
+}
+
+function renderFire() {
+
+
+}
+
+start()
